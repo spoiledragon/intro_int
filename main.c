294245699544234931063cd11a6e1f897032e1c6
spoiledragon <karlo.gallegos2031@alumnos.udg.mx>
2020-09-14 13:42:27 -0500

    ahora agregamos la edad y se regresa +1

--- main.c
+++ main.c
@@ -4,9 +4,14 @@ int main()
 {
     printf("Hola mundito \n ");
     char algo[100];
+    unsigned int edad;
+
     printf("inserta tu name\n");
     fgets(algo,sizeof(algo),stdin);
     printf("Hola %s",algo);
+    printf("Inserta tu edad \n");
+    scanf("%u",&edad);
+    printf("vas a cumplir %u", edad+1);
 
     return 0;
 }
